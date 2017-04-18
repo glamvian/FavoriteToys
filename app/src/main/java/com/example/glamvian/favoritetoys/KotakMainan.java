6b88d088d326dfc23e273161fb122c340ecce919
glamvian <glmvn.composer@gmail.com>
2017-04-19 05:23:27 +0700

    penambahan class KotakMainan

---
+++ app/src/main/java/com/example/glamvian/favoritetoys/KotakMainan.java
@@ -0,0 +1,57 @@
+package com.example.glamvian.favoritetoys;
+
+/**
+ * Created by glamvian on 19/04/17.
+ *
+ */
+
+public final class KotakMainan {
+    /**
+     * method ini return berisi 20 list  nama mainan yang sempet populer di abad 21
+     */
+    public static String[] getNamaMainan(){
+        return new String []{
+                "Pate Lele",
+                "Baksodor",
+                "Keongan",
+                "Layangan",
+                "Singitan",
+                "Nekeran",
+                "Karetan",
+                "Tembak dompes",
+                "Mercon Sreng Dor",
+                "Mercon Mbumbung",
+                "Green Soldiers",
+                "Bubbles",
+                "Spring Toy",
+                "Fortune Telling Ball",
+                "Plastic Connecting Blocks",
+                "Water Balloon",
+                "Paint-by-Numbers Kit",
+                "Tuber Head",
+                "Cool Ball with Holes in It",
+                "Toy Truck",
+                "Flying Disc",
+                "Two-Handed Pogo Stick",
+                "Toy Hoop",
+                "Dysmorphia Doll",
+                "Toy Train",
+                "Fake Vomit",
+                "Toy Telephone",
+                "Barrel of Primates",
+                "Remote Control Car",
+                "Square Puzzle Cube",
+                "Football",
+                "Intergalactic Electronic Phasers",
+                "Baby Horse Dolls",
+                "Machines that turn into other Machines",
+                "Teddy Bears",
+                "Shaved Ice Maker",
+                "Glow Stick",
+                "Squirt Guns",
+                "Miniature Replica Animals Stuffed with Beads that you swore to your parents would be worth lots of money one day",
+                "Creepy Gremlin Doll",
+                "Neodymium-Magnet Toy"
+        };
+    }
+}
