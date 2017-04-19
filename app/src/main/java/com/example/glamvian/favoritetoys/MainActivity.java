5eb87364f66ede92044c20278da38a6f5411d622
glamvian <glmvn.composer@gmail.com>
2017-04-19 22:55:56 +0700

    store list nama mainan

--- app/src/main/java/com/example/glamvian/favoritetoys/MainActivity.java
+++ app/src/main/java/com/example/glamvian/favoritetoys/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.glamvian.favoritetoys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mListToysTextview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListToysTextview = (TextView) findViewById(R.id.toys_tv_name);
+        //store data dari method static getNamaMainan di class KotakMainan
+        String [] namaMainan = KotakMainan.getNamaMainan();
+        //loop untuk mendapatkan listnama mainan dan memisahkan setiap nama mainan dengan tiga separator /n
+        for ( String mainan : namaMainan){
+            mListToysTextview.append(namaMainan+ "/n/n/n");
+        }
     }
 }
